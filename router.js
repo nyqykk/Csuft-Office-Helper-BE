c4265ecc766ad7f2da28236fbdfe10235a5ce380
umuoy1 <burningdian@gmail.com>
2021-01-18 15:13:39 +0800

    update

--- router.js
+++ router.js
@@ -1,7 +1,15 @@
+const rateLimit = require('express-rate-limit')
 const express = require('express')
 const router = express.Router()
 const getJSID = require('./controller/controller.js')
 
-router.post('/', getJSID)
+
+const limiter = rateLimit({
+    windowMs: 30 * 1000,
+    max: 100,
+    message: "Too many requests"
+})
+
+router.post('/', limiter, getJSID)
 
 module.exports = router
